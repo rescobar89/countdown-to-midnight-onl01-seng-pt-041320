7f977b02746cedb906baa38aec8dc250f6020720
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-19 22:55:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,14 +10,14 @@ end
 
 countdown(10)
 
-countdown_with_sleep(int)
-  while int > 0
-    puts "#{int} SECOND(S)!"
-    int -= 1
-    sleep 1
-  end
-  "HAPPY NEW YEAR!"
-end
+# countdown_with_sleep(int)
+#   while int > 0
+#     puts "#{int} SECOND(S)!"
+#     int -= 1
+#     sleep 1
+#   end
+#   "HAPPY NEW YEAR!"
+# end
   
-countdown_with_sleep(4)
+# countdown_with_sleep(4)
 
